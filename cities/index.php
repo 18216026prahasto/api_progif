ed9ccaa6de0a094b6c7d6e087e52951e0700c0ae
prahasto satrio utomo <18216026@std.stei.itb.ac.id>
2018-12-19 22:52:24 +0700

    THIS IS FIX

--- cities/index.php
+++ cities/index.php
@@ -2,27 +2,39 @@
     session_start();
     $connection = mysqli_connect("localhost", "root", "");
     $db = mysqli_select_db($connection, 'cities');
-    if (!isset($_GET['city'])) {
-        if (isset($_GET['country'])) {
-            $country = $_GET['country'];
-            $query = mysqli_query($connection, "SELECT * FROM city WHERE country='$country'");
-            $row = array();
-            while ($r = mysqli_fetch_assoc($query)) {
-                $row[] = $r;
+    if (isset($_SESSION['login_user'])) {
+        if (!isset($_GET['city'])) {
+            if (isset($_GET['country'])) {
+                $country = $_GET['country'];
+                $query = mysqli_query($connection, "SELECT * FROM city WHERE country='$country'");
+                $row = array();
+                while ($r = mysqli_fetch_assoc($query)) {
+                    $row[] = $r;
+                }
+                if (json_encode($row) != '[]') {
+                    print json_encode($row);
+                } else {
+                    http_response_code(404);
+                }
+            } else {
+                $query = mysqli_query($connection, "SELECT * FROM city");
+                $row = array();
+                while ($r = mysqli_fetch_assoc($query)) {
+                    $row[] = $r;
+                }
+                print json_encode($row);
             }
-            print json_encode($row);
         } else {
-            $query = mysqli_query($connection, "SELECT * FROM city");
-            $row = array();
-            while ($r = mysqli_fetch_assoc($query)) {
-                $row[] = $r;
+            $city = $_GET['city'];
+            $query = mysqli_query($connection, "SELECT * FROM city WHERE name='$city'");
+            $row = mysqli_fetch_assoc($query);
+            if (json_encode($row) != 'null') {
+                print json_encode($row);
+            } else {
+                http_response_code(404);
             }
-            print json_encode($row);
         }
     } else {
-        $city = $_GET['city'];
-        $query = mysqli_query($connection, "SELECT * FROM city WHERE name='$city'");
-        $row = mysqli_fetch_assoc($query);
-        print json_encode($row);
+        http_response_code(401);
     }
 ?>
